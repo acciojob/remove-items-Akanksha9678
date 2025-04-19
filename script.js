e7e5f926fb3c60022d2f1c77843aeba913a264cb
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-20 02:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //your JS code here. If required.
 let form=document.querySelector("form");
-let selects=document.getElementById("colorSelector");
+let select=document.getElementById("colorSelect");
 let btn=document.querySelector("input");
 
 btn.addEventListener("click",()=>{
-	
+	let selectedIndex=select.selectedIndex;
+	if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+      }
 })
