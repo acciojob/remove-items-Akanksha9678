168a4c86635dfe9765763c00e2a039de8cbfc113
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-20 02:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let form=document.querySelector("form");
+let selects=document.getElementById("colorSelector");
+let btn=document.querySelector("input");
+
+btn.addEventListener("click",()=>{
+	
+})
